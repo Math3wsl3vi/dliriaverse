13758e2cd63c604834fea1a251e739b7f32b0c94
math3wsl3vi <levimathews9841@gmail.com>
2025-04-12 15:32:45 +0300

    La Pepa de Tangaré

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className='mt-5 p-3 grid grid-cols-1 md:grid-cols-3 gap-5 '>
       {uploadData.map((item)=>(
         <div key={item.id} className='mb-2 hover:border hover:shadow-sm hover:rounded-md py-4 cursor-pointer' >
-         <div className='flex justify-between'>
+         <div className='flex justify-between mb-2'>
          <h1 className='font-poppins pl-3'>{item.name}</h1>
          <p className='font-poppins pr-3'>{item.county}</p>
          </div>
@@ -18,16 +18,16 @@ const HomePage = () => {
             <div className='flex gap-3'>
               <div className='pl-3 grid grid-cols-2 items-center gap-1'>
               <Image src='/images/heart.png' alt={item.name} width={20} height={20} className=''/>
-              <h1 className='font-poppins text-xl'> {item.likes}</h1>
+              <h1 className='font-poppins'> {item.likes}</h1>
               </div>
               <div className='pl-3 grid grid-cols-2 items-center gap-1'>
               <Image src='/images/bookmark.png' alt={item.name} width={20} height={20} className=''/>
-              <h1 className='font-poppins text-xl'> {item.likes}</h1>
+              <h1 className='font-poppins'> {item.likes}</h1>
               </div>
             </div>
             <div className='pr-3 grid grid-cols-2 items-center gap-1'>
               <Image src='/images/star.png' alt={item.name} width={20} height={20} className=''/>
-              <h1 className='font-poppins text-xl'> {item.likes}</h1>
+              <h1 className='font-poppins'> {item.likes}</h1>
               </div>
           </div>
         </div>
